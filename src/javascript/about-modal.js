75e7299fe5d3856e85a4284ebf645952351850eb
Skuba Anatoliy <photomir.in.ua@gmail.com>
2021-12-09 17:15:46 +0200

    dev to main

---
+++ src/javascript/about-modal.js
@@ -0,0 +1,18 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-about-modal-open]'),
+    closeModalBtn: document.querySelector('[data-about-modal-close]'),
+    modal: document.querySelector('[data-about-modal]'),
+  };
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', (event) => {
+    if (event.currentTarget === event.target) {
+    toggleModal();
+    } 
+});
+  
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
